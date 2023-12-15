a1f18cebbc3fe8bde53a7ee687dcb431acfe63fe
Franchini Mattia <Franchini.19129>
2023-12-15 15:03:22 +0100

    \models\User.js: delete password field and added some default values

--- models/User.js
+++ models/User.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true }, //auto-generated
   email: { type: String, required: true, unique: true}, //auto-generated
-  password: { type: String, required: true }, //auto-generated
+  //password: { type: String, required: true }, //auto-generated -- we do google auth (we don't know user password and we don't mind)
   profile_image_url: { type: String, required: true}, //auto-generated
   registered_at: { type: Date, default: Date.now }, //auto-generated
   rooms: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Room' }],
-  teaching_review: {type: Float32Array},
-  learning_review: {type: Float32Array},
-  counter_hour_teaching:{type: Number}, //?
-  counter_hour_learning:{type: Number}, //?
+  teaching_review: {type: Number, default: 0.0}, // -- Number is correct --Default value : 0 --
+  learning_review: {type: Number, default: 0.0}, // -- Number is correct -- Default value : 0 --
+  counter_hour_teaching:{type: Number, default: 0}, //? -- Default value : 0 -- minutes counter --
+  counter_hour_learning:{type: Number, default: 0}, //? -- Default value : 0 -- minutes counter --
 });
 
 const User = mongoose.model('User', userSchema);
