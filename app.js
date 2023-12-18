231d81a9f6213c65f2b52d7d3ee1c02d0ba8db86
Mattia Franchini <franchini.mattia.studente@itispaleocapa.it>
2023-12-18 15:34:03 +0100

    \app.js: connection to database and use of user api.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const cors=require('cors')
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+require('dotenv').config();
 const app = express()
 
 const port = 3000
+
+mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors({
     origin:'*'
@@ -11,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+
+app.use('/api/user', userRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
