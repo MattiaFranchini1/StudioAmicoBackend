f56c7c06edadc8b4f1bf0e267401ae9de495c947
Marco Borelli <marco05.borelli@gmail.com>
2024-01-30 16:19:23 +0100

    \middleware\AuthMiddleware.js: updated middleware conditions

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -1,9 +1,8 @@
 // Middleware to check if the user is authenticated
 const isAuthenticated = (req, res, next) => {
-  console.log(req.user); // Log the user information for debugging (optional)
   
   // Check if the user is authenticated using Passport.js
-  if (req.isAuthenticated()) {
+  if (req.isAuthenticated() || req.body.internalRequest || req.query.internalRequest) {
       // If authenticated, proceed to the next middleware or route
       return next();
   } else {
@@ -11,5 +10,6 @@ const isAuthenticated = (req, res, next) => {
       return res.status(401).json({ error: 'Unauthorized access' });
   }
 };
+
 
 module.exports = isAuthenticated; // Export the middleware for use in other parts of the application
