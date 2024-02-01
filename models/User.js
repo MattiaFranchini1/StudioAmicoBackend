73fa577fed7433a383bba12b42a01d7dd9453677
Mattia Franchini <franchini.mattia.studente@itispaleocapa.it>
2024-02-01 14:54:31 +0100

    \models\User.js: updated, fixed and refactored schema (class, bio, valutations)

--- models/User.js
+++ models/User.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true }, //auto-generated
-  email: { type: String, required: true, unique: true}, //auto-generated
-  //password: { type: String, required: true }, //auto-generated -- we do google auth (we don't know user password and we don't mind)
-  profile_image_url: { type: String, required: true}, //auto-generated
-  registered_at: { type: Date, default: Date.now }, //auto-generated
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  profile_image_url: { type: String, required: true },
+  registered_at: { type: Date, default: Date.now },
+  class: {type: String/*, required: true*/ },
+  bio: {type: String},
   rooms: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Room' }],
-  teaching_review: {type: Number, default: 0.0}, // -- Number is correct --Default value : 0 --
-  learning_review: {type: Number, default: 0.0}, // -- Number is correct -- Default value : 0 --
-  counter_hour_teaching:{type: Number, default: 0}, //? -- Default value : 0 -- minutes counter --
-  counter_hour_learning:{type: Number, default: 0}, //? -- Default value : 0 -- minutes counter --
+  teaching_review_total_stars: { type: Number, default: 0.0 },
+  teaching_review_total_number: {type: Number, default: 0},
+  learning_review_total_stars: { type: Number, default: 0.0 },
+  learning_review_total_number: {type: Number, default: 0},
+  counter_hour_teaching: { type: Number, default: 0 },
+  counter_hour_learning: { type: Number, default: 0 },
 }, {
-  versionKey: false // added to delete __v field in the database
+  versionKey: false
 });
 
 const User = mongoose.model('User', userSchema);
