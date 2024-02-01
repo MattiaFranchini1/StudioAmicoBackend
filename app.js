43f827a37b31edcaa80dc847974f0f6cb2b4d7d6
Mattia Franchini <franchini.mattia.studente@itispaleocapa.it>
2024-02-01 14:40:18 +0100

    \app.js: updated and fixed file after merge

--- app.js
+++ app.js
@@ -1,37 +1,48 @@
-const express = require('express')
-const cors=require('cors')
+const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-
+const http = require('http');
 const userRoutes = require('./routes/user');
+const passport = require('passport');
 require('dotenv').config();
 const session = require('express-session');
-const passport = require('passport');
-const app = express()
+const MongoStore = require('connect-mongo');
+require('./auth/passport')(passport)
 
-const port = 3000
+const app = express();
+const server = http.createServer(app);
 
+const port = 3000;
+
+// Connect to MongoDB using the URI from the environment variables
 mongoose.connect(process.env.ATLAS_URI, /*{ useNewUrlParser: true, useUnifiedTopology: true }*/);
 
-app.use(cors({
-    origin:'*'
-}));
+// CORS configuration
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+
+// Express middlewares
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.ATLAS_URI })
+  })
+)
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-}));
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
-// Inizializza Passport e imposta le sessioni
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/api/user', userRoutes);
-
-app.listen(port, () => {
+// API routes
+app.use('/api/users', userRoutes);
+server.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
